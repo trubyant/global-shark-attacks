66e0d7a108b72486fa00d4e251ac8bf6c7edb01e
Anton Trublin <atrublin@gmail.com>
2025-03-25 23:59:12 -0400

    Updates

---
+++ precache-manifest.c621555fcfd9b2ff948c2badec6e9297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0227146158c9f85c60438b25e20a1427",
+    "url": "/global-shark-attacks/index.html"
+  },
+  {
+    "revision": "91c8fd1f759dc18e80ef",
+    "url": "/global-shark-attacks/static/css/main.c5b4070f.chunk.css"
+  },
+  {
+    "revision": "72ca62a528853e858e70",
+    "url": "/global-shark-attacks/static/js/2.4d64e09e.chunk.js"
+  },
+  {
+    "revision": "d711200840a0c245aacb",
+    "url": "/global-shark-attacks/static/js/3.f265485a.chunk.js"
+  },
+  {
+    "revision": "91c8fd1f759dc18e80ef",
+    "url": "/global-shark-attacks/static/js/main.241c1037.chunk.js"
+  },
+  {
+    "revision": "74d68d784b0d297b4daa",
+    "url": "/global-shark-attacks/static/js/runtime~main.c9f233b8.js"
+  }
+]);
